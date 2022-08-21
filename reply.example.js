b50df428c746ec83c101077eaad5046d138dfe28
Lost Paul <70213368+LostPaul@users.noreply.github.com>
2022-08-21 13:34:39 +0200

    Log set command fix and merged the language classes into one

--- reply.example.js
+++ reply.example.js
@@ -1,18 +1,18 @@
-"await client.getLanguage.getCommandContent('same-emoji-name', 'errors', interaction, { 'type': 'content', 'interaction': interaction }))",
-"await client.getLanguage.getCommandContent('emoji-edited', 'commonly-used', interaction, { 'type': 'content', 'interaction': interaction, 'currentEmoji': emoji})",
+"await client.Language.getCommandContent('same-emoji-name', 'errors', interaction, { 'type': 'content', 'interaction': interaction }))",
+"await client.Language.getCommandContent('emoji-edited', 'commonly-used', interaction, { 'type': 'content', 'interaction': interaction, 'currentEmoji': emoji})",
 
 
-checkEmoji(options, config.emojis.error, await client.getLanguage.getCommandContent(this.name.replaceAll(' ', '-'), ['interactions', 'slash_commands'], interaction, { 'type': 'content', 'interaction': interaction, 'autoLockRoles': roles}))
+checkEmoji(options, client.config.emojis.error, await client.Language.getCommandContent(this.name.replaceAll(' ', '-'), ['interactions', 'slash_commands'], interaction, { 'type': 'content', 'interaction': interaction, 'autoLockRoles': roles}))
  
  interaction.editReply({
     content: checkEmoji(options, client.replyEmojis["success"],
-        await client.getLanguage.getCommandContent(this.name.replaceAll(" ", "-"),
+        await client.Language.getCommandContent(this.name.replaceAll(" ", "-"),
             ["interactions", "slash_commands"], interaction, { "type": "content", "interaction": interaction, "autoLockRoles": roles })), ephemeral: true
 })
 
 cooldown.delete(`${this.name.replaceAll(/\s/g, '')}${interaction.user.id}`)
                 interaction.editReply({
                     content: checkEmoji(options, client.replyEmojis["error"],
-                        await client.getLanguage.getCommandContent("autolock-not-enabled",
+                        await client.Language.getCommandContent("autolock-not-enabled",
                             "errors", interaction, { "type": "content", "interaction": interaction })), ephemeral: true
                 })
