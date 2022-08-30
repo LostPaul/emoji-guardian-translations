1d44593a0f15f980f9556f604d4b0f74058d3e4c
Lost Paul <70213368+LostPaul@users.noreply.github.com>
2022-08-30 14:53:38 +0200

    new language

--- app-commands-source/emoji-info.js
+++ app-commands-source/emoji-info.js
@@ -1,20 +1,20 @@
 module.exports = {
-    name: "emoji info",
-    global: true,
-    command: {
-        "name": "emoji-info",
-        "type": 1,
-        "dm_permission": false,
-        "default_member_permissions": "1024",
-        "description": "Get information about an emoji",
-        "options": [
-          {
-            "type": 3,
-            "name": "emoji",
-            "description": "The emoji you want to get information about.",
-            "required": true,
-            "autocomplete": true
-          }
-        ]
-    }
+  name: "emoji info",
+  global: true,
+  command: {
+    "name": "emoji-info",
+    "type": 1,
+    "dm_permission": false,
+    "default_member_permissions": "1024",
+    "description": "Get information about an emoji",
+    "options": [
+      {
+        "type": 3,
+        "name": "emoji",
+        "description": "The emoji you want to get information about.",
+        "required": true,
+        "autocomplete": true
+      }
+    ]
+  }
 }
--- reply.example.js
+++ reply.example.js
@@ -1,18 +1,18 @@
-"await client.Language.getCommandContent('same-emoji-name', 'errors', interaction, { 'type': 'content', 'interaction': interaction }))",
-"await client.Language.getCommandContent('emoji-edited', 'commonly-used', interaction, { 'type': 'content', 'interaction': interaction, 'currentEmoji': emoji})",
+"await client.language.getCommandContent('same-emoji-name', 'errors', interaction, { 'type': 'content', 'interaction': interaction }))",
+"await client.language.getCommandContent('emoji-edited', 'commonly-used', interaction, { 'type': 'content', 'interaction': interaction, 'currentEmoji': emoji})",
 
 
-checkEmoji(options, client.config.emojis.error, await client.Language.getCommandContent(this.name.replaceAll(' ', '-'), ['interactions', 'slash_commands'], interaction, { 'type': 'content', 'interaction': interaction, 'autoLockRoles': roles}))
+checkEmoji(options, client.config.emojis.error, await client.language.getCommandContent(this.name.replaceAll(' ', '-'), ['interactions', 'slash_commands'], interaction, { 'type': 'content', 'interaction': interaction, 'autoLockRoles': roles}))
  
  interaction.editReply({
     content: checkEmoji(options, client.replyEmojis["success"],
-        await client.Language.getCommandContent(this.name.replaceAll(" ", "-"),
+        await client.language.getCommandContent(this.name.replaceAll(" ", "-"),
             ["interactions", "slash_commands"], interaction, { "type": "content", "interaction": interaction, "autoLockRoles": roles })), ephemeral: true
 })
 
 cooldown.delete(`${this.name.replaceAll(/\s/g, '')}${interaction.user.id}`)
                 interaction.editReply({
                     content: checkEmoji(options, client.replyEmojis["error"],
-                        await client.Language.getCommandContent("autolock-not-enabled",
+                        await client.language.getCommandContent("autolock-not-enabled",
                             "errors", interaction, { "type": "content", "interaction": interaction })), ephemeral: true
                 })
